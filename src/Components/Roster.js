1f354da2a97fb78ee7075054c24c1259723d597f
amcmullen <amcmullen9291@gmail.com>
2022-03-04 11:45:33 -0500

    screen.getByTestId(), expect().toBeInDocument(), and cleanup() functions

--- src/Components/Roster.js
+++ src/Components/Roster.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Roster() {
-    
+
+    const students = [
+        { id:1, firstName: "Alison Murphy", grade: 9, vehicle: true },
+        { id:2, firstName: "Carl Jefferies", grade: 5, vehicle: false },
+        { id:3, firstName: "Adam Marcus", grade: 3, vehicle: false },
+        { id:4, firstName: "Lenny Tallerman", grade: 9, vehicle: true },
+        { id:5, firstName: "Beth Roachester", grade: 12, vehicle: true },
+    ];
+
   return (
       <>
-    <div>Roster</div>
-    </>
+    <div data-testid="rosterList-1">
+        <div>Roster</div>
+        {students && (
+              <div><table>
+                <thead>
+                    <tr>
+                        <th>name</th>
+                        <th>grade</th>
+                        <th>vehicle?</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {students.map((student, id ) => (
+                    <tr key={student.id}>
+                    <td>{student.firstName}</td>
+                    <td>{student.grade}</td>
+                    <td><input type="checkbox" value={student.vehicle}/></td>
+                    </tr>
+                ))}
+                </tbody>
+        </table>
+              </div>
+            )}
+    </div>
+    </> 
   )
 }
--- src/Components/__tests__/Roster.test.js
+++ src/Components/__tests__/Roster.test.js
@@ -1,7 +1,26 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import Roster from '../Roster';
+
+afterEach (() => {
+    cleanup();
+})
+
 test('test', () => {
     expect(true).toBe(true);
 });
 
-test('test 2', () => {
-    expect("Anthony").toContain('A');
-});
+test('Should render Roster component', () => {
+    render(<Roster/>);
+    const rosterElement = screen.getByTestId('rosterList-1'); // syntax; finds data-testid="rosterList-1", NOT id=""
+    expect(rosterElement).toBeInTheDocument();  //is the element on the DOM
+    //other options...
+    expect(rosterElement).toHaveTextContent('ster');
+})
+
+test('should render student name', () => {
+    const student = { id:3, firstName: "Adam Marcus", grade: 3, vehicle: false };
+    render(<Roster/>)
+    const rosterElement = screen.getByTestId('rosterList-1');
+    expect(rosterElement).toBeInTheDocument();
+    expect(rosterElement).toHaveTextContent(student.firstName);
+})
