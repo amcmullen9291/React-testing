c47d528adbc57a9b47ad3d4b4a5fafbf08e0fb9e
amcmullen <amcmullen9291@gmail.com>
2022-03-04 09:21:31 -0500

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Roster from './Components/Roster'
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <Router>
+      <Routes>
+          <Route path="/" element={<Roster/>} />
+      </Routes>
+    </Router>  );
 }
 
 export default App;
